98a7cac1b3d3608716b95c6bf37072e16ed8bf7a
Filippo Gambarota <filippo.gambarota@gmail.com>
2021-03-01 03:04:33 +0100

    update

--- meta_analysis_presentation/compile_slides.R
+++ meta_analysis_presentation/compile_slides.R
@@ -4,6 +4,6 @@ file_name <- list.files(pattern = ".Rmd")
 
 # This script compiles RMarkdown slides both in HMTL and PDF format
 
-rmarkdown::render(file_name, params = list(pdf_button = TRUE)) # html version
-
 pagedown::chrome_print(rmarkdown::render(file_name, params = list(pdf_button = FALSE))) # pdf version
+
+rmarkdown::render(file_name, params = list(pdf_button = TRUE)) # html version
